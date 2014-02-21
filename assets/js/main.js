0dc7d1c827c137a1053a222a2089ab93ea8fef64
Daniel Beilinson <beilinson@gmail.com>
2014-02-21 21:13:00 +0400

    misc

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,7 @@
 $( document ).ready(function() {
-	$('dl.sub a').on('click', function(e) {
+	$('dl.sub a').on('click', function() {
+		$("dd.sub a.active").removeClass("active");
+		$(this).parent().addClass('active');
 		var filter = $(this).data("menu");
 		if(filter == 'all') {
 			$('a.item').toggle(true);
