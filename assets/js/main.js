9d83fc4562472aeea5d8b80fd6d7e24f8329af0d
Daniel Beilinson <beilinson@gmail.com>
2014-02-21 20:57:13 +0400

    misc

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,8 @@
 $( document ).ready(function() {
+	$('dl.sub a').on('click', function(e) {
+		console.log(this);
+		console.log(e);
+	});
 	$(window).on('hashchange', function() {
 		if(window.location.hash == '#all') {
 			$('a.item').toggle(true);
