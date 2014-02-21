24561b4458de21cd8c423fcc8c735bd28d730d08
Daniel Beilinson <beilinson@gmail.com>
2014-02-21 21:05:54 +0400

    misc

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,11 @@
 $( document ).ready(function() {
 	$('dl.sub a').on('click', function(e) {
-		console.log(this);
-		console.log(e);
-	});
-	$(window).on('hashchange', function() {
-		if(window.location.hash == '#all') {
+		var filter = $(this).data();
+		if(filter == 'all') {
 			$('a.item').toggle(true);
 		} else {
-    		$('a.item:not(' + window.location.hash.replace('#', '.') + ')').toggle(false);
-    		$(window.location.hash.replace('#', '.')).toggle(true);
+    		$('a.item:not(.' + filter + ')').toggle(false);
+    		$('a.' + filter).toggle(true);
     	}
     });
 });
