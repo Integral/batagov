65b6330dd9d96034d7839190d854f76500b2a415
Daniel Beilinson <beilinson@gmail.com>
2014-02-22 20:50:24 +0400

    misc

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,5 @@
 $( document ).ready(function() {
+	console.log($(location).attr('hash'););
 	$('dl.sub a').on('click', function() {
 		$("dl.sub dd.active").removeClass("active");
 		$(this).parent().addClass('active');
