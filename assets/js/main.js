080d2248ec11876b241fbff09454fa34436362b3
Daniel Beilinson <beilinson@gmail.com>
2014-03-01 15:30:14 +0400

    misc

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,10 @@
 $( document ).ready(function() {
 	var hash = $(location).attr('hash').replace('#','');
 	if(hash == 'composer' || hash == 'performer') {
-		$('a[data-menu=' + hash + ']').parent().addClass('active');
+		$('a[data-menu="' + hash + '"]').parent().addClass('active');
 		$('a.item:not(.' + hash + '), h2:not(.' + hash + ')').toggle(false);
     	$('a.' + hash + ', h2.' + hash).toggle(true);
 	} else {
-		$('a[data-menu=*]').parent().addClass('active');
+		$('a[data-menu="*"]').parent().addClass('active');
 	}
 });
