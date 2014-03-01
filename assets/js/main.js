dc4ca8c3e6054bd87e739075430afeaa3b39517b
Daniel Beilinson <beilinson@gmail.com>
2014-03-01 15:46:18 +0400

    misc

--- assets/js/main.js
+++
@@ -1,8 +0,0 @@
-$( document ).ready(function() {
-	var hash = $(location).attr('hash').replace('#','');
-	if(hash == 'composer' || hash == 'performer') {
-		$('a[data-menu=".' + hash + '"]').parent().addClass('active');
-	} else {
-		$('a[data-menu="*"]').parent().addClass('active');
-	}
-});
