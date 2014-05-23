197458fd4387e88fbc899bf10aec29120941dcb4
Integral <beilinson@gmail.com>
2014-05-24 00:43:36 +0400

    misc

---
+++ assets/js/main.js
@@ -0,0 +1,24 @@
+$(function(){
+  var width = window.innerWidth;
+  var height = window.innerHeight;
+  var doc = $(document);
+
+  // top link
+  $('#top').click(function(e){
+    $('body').animate({ scrollTop: 0 }, 'fast');
+    e.preventDefault();
+  });
+
+  // scrolling links
+  var added;
+  doc.scroll(function(e){
+    if (doc.scrollTop() > 5) {
+      if (added) return;
+      added = true;
+      $('body').addClass('scroll');
+    } else {
+      $('body').removeClass('scroll');
+      added = false;
+    }
+  })
+})
